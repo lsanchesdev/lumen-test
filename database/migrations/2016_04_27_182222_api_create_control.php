211a33bdd64202ad676064142c8e0513ff777ecc
Leandro Sanches <leandro.sanches@Mac-mini-de-Arca-3.local>
2016-04-27 15:26:04 -0300

    Adding migration: API Control

---
+++ database/migrations/2016_04_27_182222_api_create_control.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ApiCreateControl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('control', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('request');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('control');
+    }
+}
